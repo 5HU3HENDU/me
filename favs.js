1cb6d789587418e343c9e2ef4eaf614cef6ab5bc
Shubhendu Singh <shubhendusingh1505@gmail.com>
2020-06-05 19:59:12 +0530

    Create favs.js

---
+++ favs.js
@@ -0,0 +1,57 @@
+var favs = {
+
+	"Movies" : [ 
+
+	"Titanic",
+	"Big Hero 6",
+	"Arrival",
+	"Laal Kaptaan",
+	"Delhi Belly",
+	"The Social Network",
+	"The Prestige",
+	"The Imitation Game",
+	"Masaan",
+	"Rockstar",
+	"Wall - e".
+	"Forrest Gump",
+	"The Terminal"
+	"Wreck it Ralph",
+	"Ralph breaks the internet",
+	"Aankhodekhi"
+
+	],
+
+
+
+	"Books" : [
+
+	"Steal Like An Artist",
+	"Just Enough Research",
+	"The Art of Start",
+	"0 to 1",
+	"The Dilbert Principle",
+	"Steve Jobs - by Walter Issacson",
+	"The Code Book",
+	"Algorithms to Live By",
+	"The Shape of Ideas",
+	"Don't make me think",
+	"The hard Thing about hard things",
+	"Hooked"
+
+	],
+
+
+
+
+	"Shows" : [
+
+	"Sherlock",
+	"Game of Thrones",
+	"The Peaky Blinders",
+	"The Dilbert",
+	"Breaking Bad",
+	"Pitchers"
+
+	],
+
+};
