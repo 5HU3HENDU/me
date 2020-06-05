5b8d518877d164b31720ca0520bce15502f973d5
Shubhendu Singh <shubhendusingh1505@gmail.com>
2020-06-05 20:37:09 +0530

    Create thoughts.js

---
+++ thoughts.js
@@ -0,0 +1,19 @@
+var thoughts = {
+
+	"Thoughts" : [ 
+
+	"Live life on \"Regret Minimization Framework\".",
+	"\"Sorry\" is better than \"may I...?\"",
+	"Do bad things, with permission... people might allow it. Do good things, without permission... and ask for forgiveness later, if needed.",
+	"The only difference between success & failure is \"COURAGE\".",
+	"Manage energy, not time.",
+	"Be remembered for good.",
+	"Don't be a jerk.",
+	"People judge things backward.",
+	"Sometimes success feels like an failure.",
+	"Make 2020 a memorable year.",
+	"There's no knight without a nightmare."
+
+	]
+
+};
